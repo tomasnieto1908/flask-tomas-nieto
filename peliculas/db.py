ca5be22f911633d0870ec7b6a6afe2908173e24f
tomas nieto rossi <tomasnieto249@gmail.com>
2024-05-08 10:01:53 -0300

    agregue el sql

--- peliculas/db.py
+++ peliculas/db.py
@@ -1,16 +1,21 @@
 import sqlite3
+import os
 
 import click
 from flask import current_app, g
 
+db_folder = current_app.instance_path;
+db_name = "peliculas.sqlite"
+db_file = os.path.join(db_folder,db_name)
+db_sql_file = "datos.sql"  
 
 def get_db():
     if 'db' not in g:
         g.db = sqlite3.connect(
-            current_app.config['DATABASE'],
+            db_file,
             detect_types=sqlite3.PARSE_DECLTYPES
         )
-        g.db.row_factory = sqlite3.Row
+        g.db.row_factory = dict_factory
 
     return g.db
 
@@ -23,9 +28,13 @@ def close_db(e=None):
 
 
 def init_db():
+    try:
+        os.makedirs(db_folder)
+    except OSError:
+        pass
     db = get_db()
 
-    with current_app.open_resource('schema.sql') as f:
+    with current_app.open_resource(db_sql_file) as f:
         db.executescript(f.read().decode('utf8'))
 
 
@@ -43,3 +52,5 @@ def dict_factory(cursor, row):
  """Arma un diccionario con los valores de la fila."""
  fields = [column[0] for column in cursor.description]
  return {key: value for key, value in zip(fields, row)}
+
+ 
